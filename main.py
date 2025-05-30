719c1859ed959f68d412c28293717d4687658dff
plinecom <plinecom@gmail.com>
2025-05-30 21:13:24 +0900

    ホットピクセル検出機能を実装し、PyCharmプロジェクト構成を追加

---
+++ main.py
@@ -0,0 +1,43 @@
+# これはサンプルの Python スクリプトです。
+
+def print_hi(name):
+    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
+    print(f'Hi, {name}')  # Ctrl+F8を押すとブレークポイントを切り替えます。
+import os
+os.environ["OPENCV_IO_ENABLE_OPENEXR"] = "1"
+import cv2
+import numpy as np
+# ガター内の緑色のボタンを押すとスクリプトを実行します。
+if __name__ == '__main__':
+
+
+    # 画像を読み込み、グレースケールに変換
+    img = cv2.imread('hotpixel.exr', cv2.IMREAD_GRAYSCALE|cv2.IMREAD_UNCHANGED)
+
+    # メディアンフィルターを適用
+    median = cv2.medianBlur(img, 3)
+
+    # 元画像とメディアンフィルター適用画像の差分を計算
+    diff = cv2.absdiff(img, median)
+
+    # 差分が閾値以上のピクセルをホットピクセルと判定
+    _, hot_pixels = cv2.threshold(diff, 30.0/255.0, 1.0, cv2.THRESH_BINARY)
+    # ホットピクセルの位置を取得
+    coords_hot = np.column_stack(np.where(hot_pixels > 0))
+
+    print(f"ホットピクセルの数: {len(coords_hot)}")
+    print(coords_hot)
+    for (x, y, v) in coords_hot:
+        print(x, y, v)
+        cv2.circle(img, (y, x), 50, (0, 0, 1.0), 1)
+    cv2.circle(img, (0, 0), 50, (0, 0, 1.0), 1)
+
+    # 結果を表示
+    cv2.imshow("Hot Pixels", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    img_uint8 = (img * 255).astype(np.uint8)
+    cv2.imwrite('output.jpg', img_uint8)
+    print_hi('PyCharm')
+
+# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
